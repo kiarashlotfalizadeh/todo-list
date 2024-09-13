052e8f6d8cebaaaa5b6494daa2f94f803545a61b
Kiarash <163787116+kiarashlotfalizadeh@users.noreply.github.com>
2024-09-12 20:02:47 -0400

    Complete project

--- src/misc-functions.js
+++ src/misc-functions.js
@@ -108,3 +108,6 @@ function checkIfProjectsExist() {
 }
 
 export { addProjectToArray, displayProjects, getTaskInfo, addTaskToProject, displayTasks, clearTaskForm, checkIfProjectsExist, currentProjectObject };
+
+
+
